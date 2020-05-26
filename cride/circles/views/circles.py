9dcb91ee154dec6750044f078d73bd3a09beb014
Martin Mendoza <marttcode@gmail.com>
2020-05-26 09:50:48 -0500

    Feat: Filters Circles
    Users can search circles using /circles/<parms>/
    Can search by slug_name, name. And for specific filters can use created, ordering, members.

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -5,6 +5,10 @@ from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.circles import IsCircleAdmin
 from rest_framework import viewsets, mixins
 
+# Filters
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 # Models
 from cride.circles.models import Circle, Membership
 
@@ -21,6 +25,13 @@ class CircleViewSet(mixins.CreateModelMixin,
 
     serializer_class = CircleModelSerializer
     lookup_field = 'slug_name'
+
+    # Filters
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('slug_name', 'name')
+    ordering_fields = ('rides_offered', 'rides_taken', 'name', 'created', 'members_limit')
+    ordering = ('-members__count', '-rides_offered', '-rides_taken')
+    filter_fields = ('verified', 'is_limited')
 
     def get_queryset(self):
         """Restrict list to public-only."""
